440d832c9b812f8db991f1712cd3c8bcfa95e1ff
Jonathan Burgos Saldivia <jonathanburgoss@outlook.com>
2017-12-11 15:42:26 -0300

    Add files via upload

--- httpRQ.rb
+++ httpRQ.rb
@@ -2,40 +2,33 @@
 
 require 'benchmark'
 require 'net/http'
-require 'uri'
-require 'timeout'
 require 'optparse'
+require 'timeout'
+require 'uri'
 
 system "clear"
-
 options = {}
-
-OptionParser.new do |opts|
-	opts.banner = "\n Usage: ruby httpRQ.rb [options] [arguments...]\n"+
+OptionParser.new { |opts|
+	opts.banner = "\n Usage: ruby httpRQ.rb [options] [arguments...]\n\n"+
 	" Example: ruby httpRQ.rb -u http://www.example.com -r5 -w1 , you should always put 'http://'"
 	opts.separator ""
 	opts.version = "0.1"
-	opts.on('-r', '--repeat nTIMES', 'Repeat n times, default value is 10.') do |nveces|
-		options[:nveces] = nveces;
-	end
-	opts.on('-w', '--waith nSECONDS', 'Wait n seconds, default value is 0.') do |wseconds|
-		options[:wseconds] = wseconds;
-	end
-	opts.on('-u', '--url URL', 'Measure response time HTTP Request of certain url.') do |url|
-		options[:url] = url;
-	end
+	opts.on('-r', '--repeat nTIMES', 'Repeat n times, default value is 10.') { |nveces|
+		options[:nveces] = nveces;}
+	opts.on('-u', '--url URL', 'Measure response time HTTP Request of certain url.') { |url|
+		options[:url] = url;}
+	opts.on('-w', '--waith nSECONDS', "Wait n seconds, default value is 0.\n\n") { |wseconds|
+		options[:wseconds] = wseconds;}
 	begin
 		opts.parse!
 	rescue OptionParser::ParseError => error
 		print "\n [!] #{error}\n [!] -h or --help to show valid options.\n\n"
 		exit 1
 	end
-end
+}
 
 nveces = options[:nveces].to_i
-if nveces == 0
-	nveces = 10
-end
+nveces = 10 if nveces == 0
 
 url = options[:url]
 if url == nil
@@ -44,35 +37,29 @@ if url == nil
 end
 
 wseconds = options[:wseconds].to_i
-if wseconds == 0
-	wseconds = 0
-end
+wseconds = 0 if options[:wseconds] == 0
 
 begin
 	Benchmark.realtime {Timeout::timeout(5){Net::HTTP.get_response(URI.parse(url))}}
-rescue Timeout::Error, Errno::ECONNREFUSED, SocketError, Errno::ENETDOWN => error
-	puts
-	puts " [!] Connection refused, invalid url format or unreachable Host, -h or --help to show valid options."
-	puts
+rescue Timeout::Error, Errno::ECONNREFUSED, SocketError, Errno::ENETDOWN, Errno::ENETUNREACH => error
+	print "\n [!] Connection refused: unreachable host or invalid url format, -h or --help to show valid options.\n\n"
 	exit 1
 end
 
 print "\n httpRQ by Jonathan Burgos Saldivia > \n\n"
+puts " REQUEST".ljust(10) +" RESPONSE TIME".ljust(10)
 
 total = 0
 
-(1..nveces.to_i).each do |n|
-
+(1..nveces).each { |n|
 	begin
-		tiempo = Benchmark.realtime {Timeout::timeout(3){Net::HTTP.get_response(URI.parse(url))}}.round(5)
-	rescue Timeout::Error, Errno::ECONNREFUSED, SocketError, Errno::ENETDOWN => error
-		print " [!] Request #{n}: connection refused.\n"
-			else
-		print " [+] Request #{n}: #{tiempo} seconds.\n"
+		tiempo = Benchmark.realtime {Timeout::timeout(3){Net::HTTP.get_response(URI.parse(url))}}.round(4)
+	rescue Timeout::Error, Errno::ECONNREFUSED, SocketError, Errno::ENETDOWN, Errno::ENETUNREACH => error
+		print " #{n}".ljust(10) +" connection refused\n".ljust(10)
+	else
+		print " #{n}".ljust(10) +" #{tiempo}".ljust(9) +"seconds\n".rjust(7)
 		total += tiempo
 		sleep wseconds
 	end
-
-end
-
-puts "\n [!] Average for #{url} : #{(total / nveces.to_i).round(5)} seconds. \n\n"
+}
+puts "\n Average response time #{(total / nveces.to_i).round(4)} seconds for: #{url}\n\n"
